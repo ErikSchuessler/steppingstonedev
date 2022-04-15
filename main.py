3a202432d3797cd393922d67e2e0cd095b9b58f2
Erik Schuessler <schuesslererik@gmail.com>
2022-04-15 23:47:32 +0000

    added login and password hashing

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from app import app
 from app import db
+from app.models import Roles
+import sys
+
 
 #db.drop_all()
 #db.create_all()
+
+role1 = Roles(id=1, description='Student')
+role2 = Roles(id=2, description='Business')
+role3 = Roles(id=3, description='Administrator')
+db.session.add(role1)
+db.session.add(role2)
+db.session.add(role3)
+db.session.commit()
