07248daf97d52a107754a8e8449d3ccacc5852fe
Erik Schuessler <schuesslererik@gmail.com>
2022-04-18 01:40:08 +0000

    Cleanup main.py old code.

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from app import app
 from app import db
-#from sqlalchemy.orm import backref, relationship
-#from sqlalchemy import MetaData, Table, Column, Integer, ForeignKey, String, Text, Date, Boolean
-#from sqlalchemy.ext.declarative import declarative_base
 from app.models import Roles
 import sys
 
@@ -16,23 +13,8 @@ role3 = Roles(description='Administrator')
 db.session.add(role1)
 db.session.add(role2)
 db.session.add(role3)
+"""
 
-parent1 = Parent()
-child1 = Child(parent_id=1)
-child2 = Child(parent_id=1)
-
-parent2 = Parent()
-child3 = Child(parent_id=2)
-child4 = Child(parent_id=2)
-
-db.session.add(parent1)
-db.session.add(child1)
-db.session.add(child2)
-
-db.session.add(parent2)
-db.session.add(child3)
-db.session.add(child4)
-db.session.commit() """
-
-""" if __name__ == "__main__":
-    create_app().run() """
+#use this and 'python3 main.py' at terminal to troubleshoot import errors
+#if __name__ == "__main__":
+#create_app().run()
